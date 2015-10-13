6d3c3523c761894dc5a1d728f6e7afb944a20537
Nailgun <djkp9ik@gmail.com>
2015-10-13 23:09:09 +0300

    [fix] MongoDB on production environment https://github.com/nai1gun/jobninja/issues/5

--- src/main/java/com/nailgun/jhtest/config/DatabaseConfiguration.java
+++ src/main/java/com/nailgun/jhtest/config/DatabaseConfiguration.java
@@ -1,17 +1,20 @@
 package com.nailgun.jhtest.config;
 
 
-import com.nailgun.jhtest.config.oauth2.OAuth2AuthenticationReadConverter;
 import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.nailgun.jhtest.config.oauth2.OAuth2AuthenticationReadConverter;
 import org.mongeez.Mongeez;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
-import org.springframework.boot.autoconfigure.mongo.MongoProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
@@ -20,8 +23,9 @@ import org.springframework.data.mongodb.core.convert.CustomConversions;
 import org.springframework.data.mongodb.core.mapping.event.ValidatingMongoEventListener;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import javax.inject.Inject;
+
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Configuration
@@ -33,11 +37,16 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration  {
 
     private final Logger log = LoggerFactory.getLogger(DatabaseConfiguration.class);
 
-    @Inject
-    private Mongo mongo;
-
-    @Inject
-    private MongoProperties mongoProperties;
+    @Value("${spring.data.mongodb.host:localhost}")
+    private String host;
+    @Value("${spring.data.mongodb.port:27017}")
+    private Integer port;
+    @Value("${spring.data.mongodb.username:#{null}}")
+    private String username;
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+    @Value("${spring.data.mongodb.password:#{null}}")
+    private String password;
 
     @Bean
     public ValidatingMongoEventListener validatingMongoEventListener() {
@@ -51,12 +60,18 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration  {
 
     @Override
     protected String getDatabaseName() {
-        return mongoProperties.getDatabase();
+        return database;
     }
 
     @Override
     public Mongo mongo() throws Exception {
-        return mongo;
+        boolean isDefaultCredentials = username == null || password == null;
+        List<ServerAddress> serverAddressList = Collections.singletonList(
+            new ServerAddress(host, port));
+        return isDefaultCredentials ? new MongoClient(serverAddressList) :
+            new MongoClient(serverAddressList,
+            Collections.singletonList(MongoCredential.createCredential(
+                username, database, password.toCharArray())));
     }
 
     @Bean
@@ -69,12 +84,12 @@ public class DatabaseConfiguration extends AbstractMongoConfiguration  {
 
     @Bean
     @Profile("!" + Constants.SPRING_PROFILE_PRODUCTION)
-    public Mongeez mongeez() {
+    public Mongeez mongeez() throws Exception {
         log.debug("Configuring Mongeez");
         Mongeez mongeez = new Mongeez();
         mongeez.setFile(new ClassPathResource("/config/mongeez/master.xml"));
-        mongeez.setMongo(mongo);
-        mongeez.setDbName(mongoProperties.getDatabase());
+        mongeez.setMongo(mongo());
+        mongeez.setDbName(database);
         mongeez.process();
         return mongeez;
     }
