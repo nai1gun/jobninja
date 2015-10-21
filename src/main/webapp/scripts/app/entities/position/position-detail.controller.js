56bd50b0f816c937d7335d00053310212e715de7
Nailgun <djkp9ik@gmail.com>
2015-10-21 17:38:11 +0300

    [/] view and edit on the same state https://github.com/nai1gun/jobninja/issues/6

--- src/main/webapp/scripts/app/entities/position/position-detail.controller.js
+++ src/main/webapp/scripts/app/entities/position/position-detail.controller.js
@@ -1,8 +1,10 @@
 'use strict';
 
 angular.module('jhtestApp')
-    .controller('PositionDetailController', ['$scope', '$rootScope', '$stateParams', 'entity', 'Position', function ($scope, $rootScope, $stateParams, entity, Position) {
+    .controller('PositionDetailController', ['$scope', '$rootScope', '$stateParams', 'entity', 'Position', 'PositionState', 'DateUtils',
+        function ($scope, $rootScope, $stateParams, entity, Position, PositionState, DateUtils) {
         $scope.position = entity;
+        $scope.editing = false;
         $scope.load = function (id) {
             Position.get({id: id}, function(result) {
                 $scope.position = result;
@@ -11,4 +13,26 @@ angular.module('jhtestApp')
         $rootScope.$on('jhtestApp:positionUpdate', function(event, result) {
             $scope.position = result;
         });
+        $scope.editStart = function() {
+            $scope.$position = angular.copy($scope.position);
+            $scope.editing = true;
+        };
+        $scope.editCancel = function() {
+            $scope.position = angular.copy($scope.$position);
+            $scope.editing = false;
+        };
+        $scope.editDone = function() {
+            $scope.editing = false;
+            $scope.position.edited = DateUtils.convertLocaleDateToServer(new Date());
+            Position.update($scope.position, onSaveFinished);
+        };
+
+        PositionState.getAll(function(states) {
+            $scope.states = states;
+        });
+
+        var onSaveFinished = function (result) {
+            $scope.$emit('jhtestApp:positionUpdate', result);
+        };
+
     }]);
