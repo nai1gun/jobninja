249b6689c7f892b3a93a159a798e7f1aa7e77643
Nailgun <djkp9ik@gmail.com>
2016-05-05 22:13:24 +0200

    [/]

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -7,10 +7,6 @@ angular.module('jhtestApp')
             return chrome && !chrome.app.isInstalled;
         };
 
-        $scope.installExtension = function() {
-            chrome.webstore.install();
-        };
-
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
