ecf7c40c543beb365ee8791f7bfcec226761e2eb
Nailgun <djkp9ik@gmail.com>
2016-05-05 21:24:26 +0200

    [+] chrome extension install - https://github.com/nai1gun/jobninja/issues/40

--- src/main/webapp/scripts/app/main/main.controller.js
+++ src/main/webapp/scripts/app/main/main.controller.js
@@ -2,6 +2,15 @@
 
 angular.module('jhtestApp')
     .controller('MainController', ['$scope', 'Principal', function ($scope, Principal) {
+
+        $scope.canInstallExtension = function() {
+            return chrome && !chrome.app.isInstalled;
+        };
+
+        $scope.installExtension = function() {
+            chrome.webstore.install();
+        };
+
         Principal.identity().then(function(account) {
             $scope.account = account;
             $scope.isAuthenticated = Principal.isAuthenticated;
